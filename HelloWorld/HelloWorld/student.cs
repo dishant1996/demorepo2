a749dfaed759ad014eb330a005fff7e092920ff7
dishant1996 <dishantyashwante1996.com>
2022-10-17 20:33:35 +0530

    adding one file

---
+++ HelloWorld/HelloWorld/student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class student
+    {
+        /// <summary>
+        /// this is used to print
+        /// </summary>
+        public static void method1()
+        {
+
+        }
+        static void main(string[] args)
+        {
+            method1();
+            Console.writeline("hello world");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///
+
